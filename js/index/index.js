6f631010db825dab681dafc27054c74a361276c2
David Granados <davidgranados4@gmail.com>
2022-11-03 11:04:34 +0100

    index changes

--- js/index/index.js
+++ js/index/index.js
@@ -1,5 +1,8 @@
-function boton1(){
-    var button1 = document.addEventListener("click", ()=>{
-        console.log("clicked");
-    }
-}
+var boton1 = document.getElementById('boton1');
+var boton2 = document.getElementById('boton2');
+boton1.addEventListener('click',()=>{
+    location.href="html/game-1/game-1.html";
+});
+boton2.addEventListener('click',()=>{
+    location.href="html/game-2/game-2.html";
+});
