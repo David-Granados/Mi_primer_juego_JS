b8c9b394ad6013904e93031a70d202b8f16f7bb9
David Granados <davidgranados4@gmail.com>
2022-11-08 17:58:47 +0100

    change href

--- js/index/index.js
+++ js/index/index.js
@@ -1,4 +1,4 @@
 var boton1 = document.getElementById('boton1');
 boton1.addEventListener('click',()=>{
-    location.href="/html/game-level-1.html";
+    location.href="html/game-level-1.html";
 });
